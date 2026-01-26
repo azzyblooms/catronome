6f16b95dcab43a5f6d5cadeb3355a4a0a2461fa5
azzyblooms <azzyblooms@gmail.com>
2026-01-26 16:15:21 -0500

    metronome now plays a pulse; tempo works. only 4/4 is supported atm.

--- catronome.js
+++ catronome.js
@@ -1,8 +1,54 @@
-const tickSound = new Audio('audio/meow.mp3');
-function tick() {
-    tickSound.cloneNode(true).play();
+const strongTickSound = new Audio('audio/meow.mp3');
+const midTickSound = new Audio('audio/midhit.mp3');
+const weakTickSound = new Audio('audio/weakhit.mp3');
+const startButton = document.getElementById('startButton');
+let tempo = document.getElementById('tempo');
+let metronomeOn = 0;
+let metronomeWAIT = null;
+let beat = 1;
+function strongTick() {
+    strongTickSound.cloneNode(true).play();
 }
-document.addEventListener('click', function() {
-tick();
-console.log("SUCCESS");
+function weakTick() {
+    weakTickSound.cloneNode(true).play();
+    
+}
+function midTick() {
+    midTickSound.cloneNode(true).play();
+}
+startButton.addEventListener('click', function() {
+    metronomeOn = (metronomeOn === 1) ? 0 : 1;
+    console.log("TOGGLED " + metronomeOn);
+    function playTick() {
+            switch (beat) {
+                case 1:
+                    strongTick();
+                    break;
+                case 3:
+                    midTick();
+                    break;
+                case 2:
+                case 4:
+                    weakTick();
+                break;
+            }
+                beat++;
+                if(beat > 4) beat = 1;
+        }
+        if(metronomeOn == 1) {
+        if(tempo.value > 400) {
+            tempo.value = 400;
+        }
+        if(tempo.value < 30) {
+            tempo.value = 30;
+        }
+    playTick();
+    console.log(beat);
+    metronomeWAIT = setInterval(playTick, 60000 / tempo.value);
+} else {
+    clearInterval(metronomeWAIT);
+    metronomeWAIT = null;
+    beat = 1;
+}
 });
+
